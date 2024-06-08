9c50efdcc1ae62a932b3ac6774f81d235d22aea5
Noeurn Krol <noeurnkrol@gmail.com>
2024-06-08 13:55:25 +0700

    remove version

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
 	res.send("Hello app1 is deploying...");
 });
 
+app.get('/getting', function(req, res){
+	res.send("hello world in app1 is deployed");
+});
+
+
 app.listen(8080,function(){
 	console.log('app is running in port 8080');
 })
