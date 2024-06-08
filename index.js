78e564a55770d227683713a531a5ab66e4f3a1a8
Noeurn Krol <noeurnkrol@gmail.com>
2024-06-08 18:47:40 +0700

    docker version 1.0.2

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ app.get('/', function(req, res){
 	res.send("Hello app1 is deploying...");
 });
 
+app.get('/hello', function(req, res){
+	res.send("Welcomn is deployed");
+});
 
 app.listen(8080,function(){
 	console.log('app is running in port 8080');
 });
-
