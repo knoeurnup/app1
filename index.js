cc0ba0d0d9c558521f0aec1ff2c3bbb1b4064726
Noeurn Krol <noeurnkrol@gmail.com>
2024-06-08 15:15:52 +0700

    change to default

--- index.js
+++ index.js
@@ -5,21 +5,6 @@ const app = express();
 app.get('/', function(req, res){
 	res.send("Hello app1 is deploying...");
 });
-
-app.get('/getting', function(req, res){
-	res.send("hello world in app1 is deployed");
-});
-
-app.get('/getting1', function(req, res){
-	res.send("hello world1 in app1 is deployed");
-});
-
-app.get('/new', function(req, res){
-	res.send("hello worldnew1 in app1 is deployed");
-});
-
-
-
 app.listen(8080,function(){
 	console.log('app is running in port 8080');
 })
